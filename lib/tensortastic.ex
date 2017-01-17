a5b3fba280da965546668affdd2cb605689954e2
Jamil Bou Kheir <jamil@elbii.com>
2017-01-17 00:28:18 -0600

    add early docs

--- lib/tensortastic.ex
+++ lib/tensortastic.ex
@@ -2,16 +2,23 @@ defmodule Tensortastic do
   @moduledoc """
   Experimental Tensorflow bindings for Elixir.
 
-  Currently this is just a stub -- nothing is supported yet.
+  Disclaimer: This project is a WIP and nothing but `TF_Version()` is
+  implemented yet.
   """
 
   @on_load :init
 
+  @doc """
+  Loads the NIF file, mapping its functions to this Elixir module
+  """
   def init do
     path = :filename.join(:code.priv_dir(:tensortastic), "tensortastic_nif")
     :ok = :erlang.load_nif(path, 0)
   end
 
+  @doc """
+  Returns the linked libtensorflow.so version number as a char list
+  """
   def tf_version do
     raise "NIF tf_version/0 not implemented"
   end
